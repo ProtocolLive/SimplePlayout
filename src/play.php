78da9bb4143360349b730ae1b881b873e6f14ef9
Protocol Corporation <protocollive@outlook.com>
2026-04-12 18:36:05 -0300

    Comandos sendo enviados juntos pra evitar delay

--- src/play.php
+++ src/play.php
@@ -7,20 +7,22 @@ if($_GET['video'] === 'ENTRADA NDI'):
 else:
   $video = $_GET['video'];
 endif;
-SendData('PLAY 1-10 "' . $video . '" ' . $_GET['transicao'] . ' ' . $_GET['duracao'] . ' ' . $_GET['tween'] . ' ' . $_GET['direcao']);
+$data = 'PLAY 1-10 "' . $video . '" ' . $_GET['transicao'] . ' ' . $_GET['duracao'] . ' ' . $_GET['tween'] . ' ' . $_GET['direcao'];
 
 if($_GET['logo'] == 'true'):
-  SendData('MIXER 1-11 FILL 0.835938 0.0416667 0.117188 0.208333 0 Linear');
-  SendData('MIXER 1-11 CHROMA Green 0.34 0.44 1');
-  SendData('PLAY 1-11 "LOGO" MIX 30');
+  $data .= "\r\n" . 'MIXER 1-11 FILL 0.867188 0.0430556 0.08125 0.140278 0 Linear' . "\r\n" .
+    'MIXER 1-11 CHROMA Green 0.34 0.44 1' . "\r\n" .
+    'PLAY 1-11 "LOGO" MIX 30';
 else:
-  SendData('STOP 1-11');
+  $data .= "\r\n" . 'STOP 1-11';
 endif;
 
 if($_GET['live'] == 'true'):
-  SendData('MIXER 1-12 FILL 0.851563 0.1666667 0.078125 0.138889 0 Linear');
-  SendData('MIXER 1-12 CHROMA Green 0.34 0.44 1');
-  SendData('PLAY 1-12 "AO VIVO" MIX 30');
+  $data .= "\r\n" . 'MIXER 1-12 FILL 0.878906 0.1222222 0.0570313 0.102778 0 Linear' . "\r\n" .
+    'MIXER 1-12 CHROMA Green 0.34 0.44 1' . "\r\n" .
+    'PLAY 1-12 "AO VIVO" MIX 30';
 else:
-  SendData('STOP 1-12');
+  $data .= "\r\n" . 'STOP 1-12';
 endif;
+
+SendData($data);
