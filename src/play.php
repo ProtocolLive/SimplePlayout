ca3cb2355288cfd5009a4b23319088b905911436
Protocol Corporation <protocollive@outlook.com>
2026-04-14 23:04:22 -0300

    Aumento da camada dos logos

--- src/play.php
+++ src/play.php
@@ -10,19 +10,19 @@ endif;
 $data = 'PLAY 1-10 "' . $video . '" ' . $_GET['transicao'] . ' ' . $_GET['duracao'] . ' ' . $_GET['tween'] . ' ' . $_GET['direcao'];
 
 if($_GET['logo'] == 'true'):
-  $data .= "\r\n" . 'MIXER 1-11 FILL 0.867188 0.0430556 0.08125 0.140278 0 Linear' . "\r\n" .
-    'MIXER 1-11 CHROMA Green 0.34 0.44 1' . "\r\n" .
-    'PLAY 1-11 "LOGO" MIX 30';
+  $data .= "\r\n" . 'MIXER 1-20 FILL 0.867188 0.0430556 0.08125 0.140278 0 Linear' . "\r\n" .
+    'MIXER 1-20 CHROMA Green 0.34 0.44 1' . "\r\n" .
+    'PLAY 1-20 "LOGO" MIX 30';
 else:
-  $data .= "\r\n" . 'STOP 1-11';
+  $data .= "\r\n" . 'STOP 1-20';
 endif;
 
 if($_GET['live'] == 'true'):
-  $data .= "\r\n" . 'MIXER 1-12 FILL 0.878906 0.1222222 0.0570313 0.102778 0 Linear' . "\r\n" .
-    'MIXER 1-12 CHROMA Green 0.34 0.44 1' . "\r\n" .
-    'PLAY 1-12 "AO VIVO" MIX 30';
+  $data .= "\r\n" . 'MIXER 1-21 FILL 0.878906 0.1222222 0.0570313 0.102778 0 Linear' . "\r\n" .
+    'MIXER 1-21 CHROMA Green 0.34 0.44 1' . "\r\n" .
+    'PLAY 1-21 "AO VIVO" MIX 30';
 else:
-  $data .= "\r\n" . 'STOP 1-12';
+  $data .= "\r\n" . 'STOP 1-21';
 endif;
 
 SendData($data);
