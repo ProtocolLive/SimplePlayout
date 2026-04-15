9d36a7e1131eb828b7b8c3edfe6e275e1b32c6f0
Protocol Corporation <protocollive@outlook.com>
2026-04-15 01:38:28 -0300

    Adicionado layer de GC por NDI

--- src/config.php
+++ src/config.php
@@ -2,12 +2,14 @@
 $action = $_GET['action'] ?? 'get';
 
 if($action === 'save'):
+  $_POST = array_map(trim(...), $_POST);
   file_put_contents('config.json', json_encode($_POST));
 else:
   $config = json_decode(file_get_contents('config.json'), true);
   $html = file_get_contents('config.htm');
   $html = str_replace('**SERVER**', $config['server'] ?? '', $html);
   $html = str_replace('**NDI**', $config['ndi'] ?? '', $html);
+  $html = str_replace('**NDI2**', $config['ndi2'] ?? '', $html);
   $html = str_replace('**RTMP**', $config['rtmp'] ?? '', $html);
   $html = str_replace('**RTMP_KEY**', $config['rtmp_key'] ?? '', $html);
   echo $html;
--- src/play.php
+++ src/play.php
@@ -1,28 +1,43 @@
 <?php
 require('SendData.php');
 
-if($_GET['video'] === 'ENTRADA NDI'):
-  $config = json_decode(file_get_contents('config.json'), true);
-  $video = 'ndi://' . $config['ndi'];
-else:
-  $video = $_GET['video'];
-endif;
-$data = 'PLAY 1-10 "' . $video . '" ' . $_GET['transicao'] . ' ' . $_GET['duracao'] . ' ' . $_GET['tween'] . ' ' . $_GET['direcao'];
-
-if($_GET['logo'] == 'true'):
-  $data .= "\r\n" . 'MIXER 1-20 FILL 0.867188 0.0430556 0.08125 0.140278 0 Linear' . "\r\n" .
-    'MIXER 1-20 CHROMA Green 0.34 0.44 1' . "\r\n" .
-    'PLAY 1-20 "LOGO" MIX 30';
-else:
-  $data .= "\r\n" . 'STOP 1-20';
+$config = json_decode(file_get_contents('config.json'), true);
+if(isset($_GET['video'])):
+  if($_GET['video'] === 'ENTRADA NDI'):
+    $video = 'ndi://' . $config['ndi'];
+  else:
+    $video = $_GET['video'];
+  endif;
+  $data = 'PLAY 1-10 "' . $video . '" ' . $_GET['transicao'] . ' ' . $_GET['duracao'] . ' ' . $_GET['tween'] . ' ' . $_GET['direcao'];
 endif;
 
-if($_GET['live'] == 'true'):
-  $data .= "\r\n" . 'MIXER 1-21 FILL 0.878906 0.1222222 0.0570313 0.102778 0 Linear' . "\r\n" .
-    'MIXER 1-21 CHROMA Green 0.34 0.44 1' . "\r\n" .
-    'PLAY 1-21 "AO VIVO" MIX 30';
-else:
-  $data .= "\r\n" . 'STOP 1-21';
+if(isset($_GET['gc'])):
+  if($_GET['gc'] === 'true'):
+    $data = 'PLAY 1-11 "ndi://' . $config['ndi2'] . '"' . "\r\n";
+    $data .= 'MIXER 1-11 CHROMA Green 0.34 0.44 1';
+  else:
+    $data = 'STOP 1-11';
+  endif;
+endif;
+
+if(isset($_GET['logo'])):
+  if($_GET['logo'] == 'true'):
+    $data .= "\r\n" . 'MIXER 1-20 FILL 0.867188 0.0430556 0.08125 0.140278 0 Linear' . "\r\n";
+    $data .= 'MIXER 1-20 CHROMA Green 0.34 0.44 1' . "\r\n";
+    $data .= 'PLAY 1-20 "LOGO" MIX 30';
+  else:
+    $data .= "\r\n" . 'STOP 1-20';
+  endif;
+endif;
+
+if(isset($_GET['live'])):
+  if($_GET['live'] == 'true'):
+    $data .= "\r\n" . 'MIXER 1-21 FILL 0.878906 0.1222222 0.0570313 0.102778 0 Linear' . "\r\n";
+    $data .= 'MIXER 1-21 CHROMA Green 0.34 0.44 1' . "\r\n";
+    $data .= 'PLAY 1-21 "AO VIVO" MIX 30';
+  else:
+    $data .= "\r\n" . 'STOP 1-21';
+  endif;
 endif;
 
 SendData($data);
