cb08e2679f3ca47e2f1c09e3b27094b7775a654a
Protocol Corporation <protocollive@outlook.com>
2026-06-07 12:29:26 -0300

    Separação dos dados no array

--- src/osc.php
+++ src/osc.php
@@ -12,26 +12,23 @@ header('Connection:keep-alive');
 
 $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 $temp = socket_bind($sock, gethostbyname(gethostname()), 6250);
-while(true):
+while(true){
   socket_recvfrom($sock, $buffer, 4096, 0, $from, $portFrom);
-  $mensagens = ParseOsc($buffer);
-  $index = array_search('/channel/1/stage/layer/10/foreground/file/name', array_column($mensagens, 'address'));
-  $video = $index === false ? null : $mensagens[$index]['args'][0];
-  $index = array_search('/channel/1/stage/layer/10/foreground/file/time', array_column($mensagens, 'address'));
-  $tempo = $index === false ? null : $mensagens[$index]['args'][0];
-
-  if($video !== null and $tempo !== null):
-    echo "event: message\n";
-    echo 'data: ["' . $video . '",' . $tempo . ']' . "\n\n";
+  $dados = parseOscToNestedArray($buffer);
+  if(empty($dados)):
+    continue;
   endif;
-endwhile;
+  if(isset($dados['channel'][1]['stage']['layer'][10]['foreground']['file']['name'])
+  and isset($dados['channel'][1]['stage']['layer'][10]['foreground']['file']['time'])):
+    echo "event: message\n";
+    echo 'data: ["' . $dados['channel'][1]['stage']['layer'][10]['foreground']['file']['name'] . '",' .
+      $dados['channel'][1]['stage']['layer'][10]['foreground']['file']['time'][0] . ']' . "\n\n";
+  endif;
+}
 
-function ParseOsc(
-  string $buffer,
-  &$offset = 0
-):array{
+function parseOscToArray($buffer, &$offset = 0){
   $results = [];
-  $header = OscString($buffer, $offset);
+  $header = readOscString($buffer, $offset);
   if($header === "#bundle"):
     $offset += 8;
     while($offset < strlen($buffer)):
@@ -39,43 +36,65 @@ function ParseOsc(
       $offset += 4;
       $elementData = substr($buffer, $offset, $elementSize);
       $subOffset = 0;
-      $results = array_merge($results, ParseOsc($elementData, $subOffset));
+      $results = array_merge($results, parseOscToArray($elementData, $subOffset));
       $offset += $elementSize;
     endwhile;
   else:
     $address = $header;
-    $tags = OscString($buffer, $offset);
+    $tags = readOscString($buffer, $offset);
     $args = [];
     if(str_starts_with($tags, ',')):
       $tagArray = str_split(substr($tags, 1));
       foreach($tagArray as $tag):
         if($tag === 'f'):
-          $val = unpack("f", strrev(substr($buffer, $offset, 4)))[1];
+          $val = unpack('f', strrev(substr($buffer, $offset, 4)))[1];
           $args[] = round($val, 4);
           $offset += 4;
         elseif($tag === 'i'):
-          $val = unpack("N", substr($buffer, $offset, 4))[1];
-          if ($val > 0x7FFFFFFF) $val -= 0x100000000;
+          $val = unpack('N', substr($buffer, $offset, 4))[1];
+          if($val > 0x7FFFFFFF):
+            $val -= 0x100000000;
+          endif;
           $args[] = $val;
           $offset += 4;
         elseif($tag === 's'):
-          $args[] = OscString($buffer, $offset);
+          $args[] = readOscString($buffer, $offset);
         endif;
       endforeach;
     endif;
     $results[] = [
       'address' => $address,
-      'tags' => $tags,
       'args' => $args
     ];
   endif;
   return $results;
 }
 
-function OscString(
-  string $buffer,
-  &$offset
-):string{
+function parseOscToNestedArray($buffer){
+  $mensagensFlat = parseOscToArray($buffer);
+  $tree = [];
+  foreach($mensagensFlat as $msg):
+    $pathStr = ltrim($msg['address'], '/');
+    if(empty($pathStr)):
+      continue;
+    endif;
+    $parts = explode('/', $pathStr);
+    $current = &$tree;
+    foreach($parts as $part):
+      if(is_numeric($part)):
+        $part = (int) $part;
+      endif;
+      if(!isset($current[$part])):
+        $current[$part] = [];
+      endif;
+      $current = &$current[$part];
+    endforeach;
+    $current = (count($msg['args']) === 1) ? $msg['args'][0] : $msg['args'];
+  endforeach;
+  return $tree;
+}
+
+function readOscString($buffer, &$offset){
   $nullPos = strpos($buffer, "\0", $offset);
   if($nullPos === false):
     return '';
